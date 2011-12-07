efd1188d50c89a1da83fa39638f193f142b7130c
Roman Yevstihnyeyev <ryevstihnyeyev@college.harvard.edu>
2011-12-06 20:02:44 -0500

    add all files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CS50Projects::Application.config.secret_token = '2564d549fd369bd9811dd76117f222dd50f89200107291e4f89c4c5850ef6577afc2070589060c7e673a2815b94ef52f1aaca75897ba1859adb0fc2a915a95a5'
