69685a81c31400e0410bdaf5bd78501bd6611ff1
Roman Yevstihnyeyev <ryevstihnyeyev@college.harvard.edu>
2011-12-08 03:28:02 -0500

    Final touches on Add Project

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,8 @@
 class PagesController < ApplicationController
   def home
     @title = "Home"
+    # Fetch all of the projects to display on the home page, using pagination.
+    @projects = Project.paginate(:page => params[:page])
   end
   
   def about
